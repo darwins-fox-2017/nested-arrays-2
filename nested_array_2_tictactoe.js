c6eace2d77b97749af36c68a91b7cd70ef722db6
adamsaparudin <adamsaparudin@gmail.com>
2017-02-01 15:01:08 +0700

    SUDAH SELESAI TERAKHIR

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,28 +1,27 @@
 'use strict'
 function tic_tac_toe_board () {
   let arr = [];
+  let xCount = 0;
+  let oCount = 0;
   for(let i=0; i<3; i++) {
     arr[i] = []
     for(let j=0; j<3; j++) {
       let filler = Math.floor(Math.random() * 10)
-      let xCount = 0;
-      let oCount = 0;
-      if (xCount > 4) {
+      if (xCount === 5)
         arr[i][j] = 'o';
-      }
-      else if (oCount > 4) {
+      else if (oCount === 5)
         arr[i][j] = 'x';
-      }
       else if (filler % 2 === 0) {
         arr[i][j] = 'x';
-        xCount++;
-      } else {
+        xCount += 1;
+      }
+      else if (filler % 2 !== 0) {
         arr[i][j] = 'o';
-        oCount++;
+        oCount += 1;
       }
     }
   }
-  return arr;
+return arr;
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
