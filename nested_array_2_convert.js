eaa8601f602efa1be69060b52dbdb881bb3292ca
Bambang Handoko <email": null>
2017-02-01 21:13:30 +0700

    Nested Array 2 Done

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -10,7 +10,16 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-    // your convert code here
+
+  let arr = []
+  for (let i = 1; i<nestedArray.length; i++) {
+    var obj = {}
+    for (let j = 0; j<nestedArray[i].length; j++) {
+      obj[nestedArray[0][j]] = nestedArray[i][j]
+    }
+    arr.push(obj)
+  }
+  return arr
 }
 
 let object_roster = convert_roster_format(roster)
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,30 @@
 function tic_tac_toe_board () {
-  // ...
+  let x = 0
+  let o = 0
+  let board = [[], [], []]
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j=0; j < board.length; j++) {
+
+      let randomPin = Math.random();
+      if ( x<5 && o<5) {
+        if ( randomPin > 0.5) {
+          x++
+          board[i].push('x')
+        } else {
+          o++
+          board[i].push('o')
+        }
+      } else {
+        if (x === 5) {
+          board[i].push('x')
+        } else {
+          board[i].push('o')
+        }
+      }
+    }
+  }
+  return board
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
