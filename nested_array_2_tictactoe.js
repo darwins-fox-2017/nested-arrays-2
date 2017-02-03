4f8bdede349b4ec8ee6ef5c6c8f1f707f90c8541
irwinpratajaya <irwinpratajaya@gmail.com>
2017-02-03 10:51:26 +0700

    tictactoe

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,32 @@
 function tic_tac_toe_board () {
   // ...
+  var xo_random = ["X", "O"]
+  var board = [];
+  var countX = 0;
+  var countO = 0;
+
+  for (var i = 0;i < 3; i++) {
+    var arr = []
+    board.push(arr)
+    board[i].push(xo_random[Math.floor(Math.random()* 2)]);
+    board[i].push(xo_random[Math.floor(Math.random()* 2)]);
+    board[i].push(xo_random[Math.floor(Math.random()* 2)]);
+  }
+
+  for (var i = 0;i < 3;i++) {
+    for(var j = 0;j < 3;j++) {
+      if (board[i][j] == "X") {
+        countX++
+      } else {
+        countO++
+      }
+    }
+  }
+  if (countX > 5 || countO > 5) {
+    return  tic_tac_toe_board()
+  } else {
+    return board;
+  }
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
