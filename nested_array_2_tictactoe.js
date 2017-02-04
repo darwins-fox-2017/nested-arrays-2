15b4ee277b1dbfc052ba4a2649b7fa21dbcf7dec
danilags <aguspermadi94@gmail.com>
2017-02-04 21:00:07 +0700

    Daniel Nested Array 2

--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,25 @@
 function tic_tac_toe_board () {
   // ...
+  let arr = []
+
+  let data = ['X', 'O']
+  
+  for (let i = 0; i < 3; i++) {
+    arr.push([])
+    // cetak baris pertama
+    let acak = Math.floor(Math.random() * 2 ) // 0 ? 1
+    arr[i][0] = data[acak]
+
+    for (let j = 1; j < 3; j++) {
+      if(arr[i][j-1] === 'O') {
+          arr[i][j] = 'X'
+      } else { 
+        arr[i][j] = 'O'
+      }
+    } 
+  }
+
+  return arr 
 }
 
 console.log(tic_tac_toe_board()) // => make a random tic tic board
