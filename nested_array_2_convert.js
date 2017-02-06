0e67676c8efb38a75c3dd2450cd5719f1ba98101
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-06 11:07:48 +0700

    nested arr2

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -1,16 +1,30 @@
-let roster = [
-  ['Number', 'Name', 'Position', 'Points per Game'],
-  [12, 'Joe Schmo', 'Center', [14, 32, 7, 0, 23] ],
-  [9, 'Ms. Buckets', 'Point Guard', [19, 0, 11, 22, 0] ],
-  [31, 'Harvey Kay', 'Shooting Guard', [0, 30, 16, 0, 25] ],
-  [7, 'Sally Talls', 'Power Forward ', [18, 29, 26, 31, 19] ],
-  [22, 'MK DiBoux ', 'Small Forward ', [11, 0, 23, 17, 0] ]
+var roster = [
+    ["Number", "Name", "Position", "Points per Game"],
+    ["12", "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
+    ["9", "Ms. Buckets ", "Point Guard", [19, 0, 11, 22, 0]],
+    ["31", "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
+    ["7", "Sally Talls", "Power Forward ", [18, 29, 26, 31, 19]],
+    ["22", "MK DiBoux ", "Small Forward ", [11, 0, 23, 17, 0]]
 ]
 
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
-function convert_roster_format (nestedArray) {
-    // your convert code here
+function convert_roster_format(nestedArray) {
+  var arraySementara = []
+  for (var i = 0; i < roster.length; i++) {
+    if (i != 0 ) {
+      var tampung = {}
+      tampung[roster[0][0]] = roster[i][0]
+      tampung[roster[0][1]] = roster[i][1]
+      tampung[roster[0][2]] = roster[i][2]
+      tampung[roster[0][3]] = roster[i][3]
+
+      // console.log(tampung);
+      arraySementara.push(tampung)
+    }
+  }
+  // console.log(arraySementara);
+  return arraySementara
 }
 
 let object_roster = convert_roster_format(roster)
--- nested_array_2_tictactoe.js
+++ nested_array_2_tictactoe.js
@@ -1,5 +1,26 @@
-function tic_tac_toe_board () {
-  // ...
+
+function tic_tac_toe_board() {
+  var temp = []
+  for (var i = 0; i < 3; i++) {
+    var random = Math.random()
+    if (random > 0.5) {
+      temp.push('X')
+    }else if (random < 0.5) {
+      temp.push('O')
+    }
+  }
+  return temp
 }
 
-console.log(tic_tac_toe_board()) // => make a random tic tic board
+function newTable() {
+  for (var i = 0; i < 6; i++) {
+    console.log(tic_tac_toe_board());
+    // return tic_tac_toe_board()
+  }
+}
+
+
+
+// console.log(tic_tac_toe_board()) // => make a random tic tic board
+newTable()
+// console.log(newTable());
